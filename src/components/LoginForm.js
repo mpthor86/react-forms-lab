3358b3972187667801a34124845262076b0c5804
michael <mpthornton86@gmail.com>
2021-03-01 14:33:33 -0700

    commit tree 172b5d83378fad851f5ff426746d07952c45812b

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,19 +7,34 @@ class LoginForm extends React.Component {
     this.state = {};
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if(this.state.username && this.state.password){
+    this.props.handleLogin(this.state)
+  }}
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text"
+            value={this.state.username}
+            onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password"
+            onChange={this.handleChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {char: 280};
+  }
+
+  handleChange = e => {
+    console.log(e.target.value)
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+
+    this.setState(prevState => {
+      return {char: prevState.char - 1}
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" 
+        value={this.state.message}
+        onChange={this.handleChange} />
+        {this.state.char}
       </div>
     );
   }
